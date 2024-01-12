ff8d65e02eff64f8ae8a86492458b1a0c95ee510
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-01-12 01:08:45 +0100

    creando el header

---
+++ components/widgets/Header.tsx
@@ -0,0 +1,13 @@
+
+
+export const Header = () => {
+  return (
+    <header className="fixed w-full left-0 top-0 z-0">
+      <div className="container mx-auto flex items-center justify-between p-5 lg:py-5 lg:px-0">
+        <section>LOGO</section>
+        <section>NAVBAR</section>
+      </div>
+
+    </header>
+  )
+}
